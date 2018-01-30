ea2a6ed0e2badafd1f604435cb3f357c8653eae2
Rizwanul Haque <mrizwan701@gmail.com>
2018-01-30 19:30:28 +0530

    some changes

---
+++ src/Test/DDD.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class DDD {
+
+	public static void main(String[] args) {
+
+		String str=null;
+		
+		if(str!=null & str.length()>0)
+		{
+			System.out.println("hello");
+		}
+	}
+}
